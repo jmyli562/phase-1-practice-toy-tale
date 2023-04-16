356c4d6133248dbc5caa3adfdebffe22a37596d7
Jimmy Li <jmyli562@gmail.com>
2023-04-16 13:36:22 -0700

    created GET request

--- src/index.js
+++ src/index.js
@@ -12,4 +12,20 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetch("http://localhost:3000/toys")
+  .then(res => res.json())
+  .then(function(data){
+    data.forEach((toy) => {
+    const toyCollection = document.getElementById("toy-collection");
+    const newToy = document.createElement("div")
+    newToy.className = "card";
+    newToy.innerHTML = `
+      <h2>${toy.name}</h2>
+      <img src="${toy.image}" class="toy-avatar" />
+      <p>${toy.likes}</p>
+      <button class="like-btn" id="${toy.id}">Like ❤️</button>`
+    toyCollection.appendChild(newToy)
+    })
+  })
 });
