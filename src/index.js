54c7af0703f1355fe5e92d5cfe6726b518c80287
Jimmy Li <jmyli562@gmail.com>
2023-04-16 14:08:10 -0700

    adds POST request to add new Toy to DOM

--- src/index.js
+++ src/index.js
@@ -28,4 +28,40 @@ document.addEventListener("DOMContentLoaded", () => {
     toyCollection.appendChild(newToy)
     })
   })
+  //we need to grab the input from the form
+  const form = document.getElementsByClassName("add-toy-form")[0];
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault(); //prevents the form from refreshing when the info is submitted
+    const toyName = document.getElementsByClassName("input-text")[0].value; //grabs the info in the first collection
+    const toyURL = document.getElementsByClassName("input-text")[1].value //grabs the info in the second collection
+
+    return fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        "name": `${toyName}`,
+        "image": `${toyURL}`,
+        "likes": 0 
+      })
+    })
+    .then(function(response){
+      return response.json()
+    })
+    .then(function(data){
+    const toyCollection = document.getElementById("toy-collection");
+    const newToy = document.createElement("div")
+    newToy.className = "card";
+    newToy.innerHTML = `
+      <h2>${data.name}</h2>
+      <img src="${data.image}" class="toy-avatar" />
+      <p>${data.likes}</p>
+      <button class="like-btn" id="${data.id}">Like ❤️</button>`
+    toyCollection.appendChild(newToy)
+    form.reset();
+    })
+  })
 });
